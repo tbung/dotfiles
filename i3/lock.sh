285d4bb60fa94a0caf0d5fabe3d65f71d1339a8e
Till Bungert <tillbungert@gmail.com>
2018-05-02 18:29:27 +0200

    Improve lock screen

--- i3/lock.sh
+++ i3/lock.sh
@@ -6,6 +6,7 @@ tmpbg='/tmp/screen.png'
 (( $# )) && { icon=$1; }
 
 scrot "$tmpbg"
-convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
+# convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
+convert "$tmpbg" -blur 0x6 "$tmpbg"
 convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
 i3lock -u -i "$tmpbg"
