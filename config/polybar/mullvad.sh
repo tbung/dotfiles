cb2d8a618ef752870f46f4d22900a7597351ab35
Till Bungert <tillbungert@gmail.com>
2020-11-20 21:13:26 +0100

    Change polybar

--- config/polybar/mullvad.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-DISPLAY_CHAR=""
-if [[ $(ip addr) == *mullvad* ]]; then
-    echo $DISPLAY_CHAR
-else
-    echo ""
-fi
--- config/polybar/urz-vpn.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-DISPLAY_CHAR=""
-if [[ $(pgrep openconnect) ]]; then
-    echo $DISPLAY_CHAR
-else
-    echo ""
-fi
