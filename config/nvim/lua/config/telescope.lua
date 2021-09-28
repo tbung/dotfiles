2213c2ece23965afe46cb4184febab11cdcbd0d5
Till Bungert <till.bungert@dkfz-heidelberg.de>
2021-09-28 17:43:50 +0200

    Add better telescope mappings

--- config/nvim/lua/config/telescope.lua
+++ config/nvim/lua/config/telescope.lua
@@ -1,3 +1,11 @@
-require('telescope').setup{}
+require('telescope').setup{
+    extensions = {
+        project = {
+            base_dirs = {
+                '~/Projects',
+            }
+        }
+    }
+}
 require('telescope').load_extension('project')
 require("telescope").load_extension("git_worktree")
