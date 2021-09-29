2bbd9294bd33199ec40d5621d38de17214e0a279
Till Bungert <tillbungert@gmail.com>
2021-09-29 20:39:08 +0200

    Add systray

--- config/polybar/scripts/toggle.sh
+++ config/polybar/scripts/toggle.sh
@@ -3,9 +3,9 @@
 if (($(bspc config -m eDP1 bottom_padding) > 0))
 then
     # Polybar should be visible, let's hide it and reset bottom padding
-    polybar-msg cmd hide && bspc config bottom_padding 0
+    polybar-msg cmd hide && bspc config bottom_padding 0 && bspc config gapless_monocle true
 else
     # Polybar should be hidden, let's show it, bspwm should automatically
     # adjust padding
-    polybar-msg cmd show
+    polybar-msg cmd show && bspc config gapless_monocle false
 fi
