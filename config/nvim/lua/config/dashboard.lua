1127e585475f77891621933b1faea297a8284817
Till Bungert <tillbungert@gmail.com>
2021-09-30 21:28:47 +0200

    Switch to lightspeed

--- config/nvim/lua/config/dashboard.lua
+++ config/nvim/lua/config/dashboard.lua
@@ -1,9 +1,10 @@
+local home = os.getenv('HOME')
 vim.g.dashboard_default_executive = "telescope"
--- vim.g.dashboard_preview_command = "splashcii"
-vim.g.dashboard_preview_pipeline = "lolcat"
--- vim.g.dashboard_preview_file = "beach"
--- vim.g.dashboard_preview_file_height = 20
--- vim.g.dashboard_preview_file_width = 80
+-- vim.g.dashboard_preview_command = "cat"
+-- vim.g.dashboard_preview_pipeline = "lolcat"
+-- vim.g.dashboard_preview_file = home .. '/.dotfiles/lolcat'
+-- vim.g.dashboard_preview_file_height = 12
+-- vim.g.dashboard_preview_file_width = 55
 vim.g.dashboard_enable_session = 0
 vim.g.dashboard_disable_statusline = 0
 
