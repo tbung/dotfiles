26f56c47810730a0ae9517a4ecb6abec837eb040
Till Bungert <tillbungert@gmail.com>
2021-11-26 13:05:26 +0100

    Switch to vim.opt and add harpoon keybinding

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -1,87 +1,87 @@
 -- Always use utf-8
-vim.o.encoding = 'utf-8'
+vim.opt.encoding = 'utf-8'
 
 -- Display line numbers relative to current line, makes moving easier
-vim.wo.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Display real line number on current line
-vim.wo.number = true
+vim.opt.number = true
 
 -- Highlight found stuff while still searching
-vim.o.incsearch = true
+vim.opt.incsearch = true
 
 -- Spaces > tabs
-vim.o.expandtab = true
+vim.opt.expandtab = true
 
 -- 4 spaces = 1 tab
-vim.o.shiftwidth = 4
-vim.o.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
 
 -- Display current position in bottom right
-vim.o.ruler = true
+vim.opt.ruler = true
 
 -- Autoindent based on previous line
-vim.o.autoindent = true
+vim.opt.autoindent = true
 
 -- Autoindent based on filetype
 -- filetype plugin indent on
 
 -- Make backspace behave as expected
-vim.o.backspace =  'indent,eol,start'
+vim.opt.backspace =  'indent,eol,start'
 
 -- Syntax highlighting
 -- syntax on
 
 -- Stop vim from waiting for another cmd when pressing esc
--- vim.o.timeout =truetimeoutlen=3000 ttimeoutlen=100
+-- vim.opt.timeout =truetimeoutlen=3000 ttimeoutlen=100
 
 -- Disable warning bell
--- vim.o.noerrorbells visualbell t_vb=
--- autocmd GUIEnter * vim.o.visualbell t_vb=
-vim.o.belloff = 'all'
+-- vim.opt.noerrorbells visualbell t_vb=
+-- autocmd GUIEnter * vim.opt.visualbell t_vb=
+vim.opt.belloff = 'all'
 
 -- Auto-linebreak after n characters
--- vim.o.textwidth = 79
-vim.o.wrap = false
-vim.o.signcolumn = "yes"
+-- vim.opt.textwidth = 79
+vim.opt.wrap = false
+vim.opt.signcolumn = "yes"
 
 -- Always keep 2 lines after cursor
-vim.o.scrolloff = 2
+vim.opt.scrolloff = 2
 
 -- Use LF as line ending character
--- vim.o.fileformat = unix
--- vim.o.fileformats=unix,dos
+-- vim.opt.fileformat = unix
+-- vim.opt.fileformats=unix,dos
 
 -- Disable swap files
-vim.o.swapfile = false
+vim.opt.swapfile = false
 
 -- Allow switching between buffers without having to save
-vim.o.hidden = true
+vim.opt.hidden = true
 
 -- Since the cursor tells us the mode, vim does not need to
-vim.o.showmode = false
+vim.opt.showmode = false
 
 -- Fold by default
--- vim.o.foldmethod=syntax
-vim.wo.foldmethod = 'expr'
-vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
-vim.wo.foldtext = "getline(v:foldstart).'...'.trim(getline(v:foldend))"
-vim.wo.fillchars = [[fold: ]]
-vim.wo.foldnestmax = 3
-vim.wo.foldminlines = 1
+-- vim.opt.foldmethod=syntax
+vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.opt.foldtext = "getline(v:foldstart).'...'.trim(getline(v:foldend))"
+vim.opt.fillchars = [[fold: ]]
+vim.opt.foldnestmax = 3
+vim.opt.foldminlines = 1
 
 -- Apparently faster
-vim.o.regexpengine = 0
+vim.opt.regexpengine = 0
 
 -- Intuitive splits (bottom/right)
-vim.o.splitbelow = true
-vim.o.splitright = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
 
 -- Colors
 -- let &t_8f = --38;2;%lu;%lu;%lum--
 -- let &t_8b = --48;2;%lu;%lu;%lum--
-vim.o.termguicolors = true
-vim.o.background = 'dark'
+vim.opt.termguicolors = true
+vim.opt.background = 'dark'
 -- vim.g.colors_name = 'snazzy'
 -- vim.g.tokyonight_style = "storm"
 vim.g.tokyonight_style = "night"
@@ -93,12 +93,12 @@ vim.cmd('colorscheme tokyonight')
 -- vim.g.sonokai_diagnostic_virtual_text = 'colored'
 -- vim.cmd('colorscheme sonokai')
 
-vim.o.completeopt = 'menuone,noinsert,noselect'
+vim.opt.completeopt = 'menuone,noinsert,noselect'
 -- Don't show the dumb matching stuff.
 vim.cmd [[set shortmess+=c]]
 
-vim.wo.spell = true
-vim.o.spelllang = 'en,de'
+vim.opt.spell = true
+vim.opt.spelllang = 'en,de'
 
 function StatusLine()
     local statusline = table.concat({
@@ -123,4 +123,4 @@ function StatusLine()
     return statusline
 end
 
-vim.o.statusline = "%!luaeval('StatusLine()')"
+vim.opt.statusline = "%!luaeval('StatusLine()')"
