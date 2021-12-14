22ebe9216cb2eafcf1de6583f7556ae1e4bfb103
Till Bungert <tillbungert@gmail.com>
2021-12-14 16:42:06 +0100

    feat: switch to a simple install script
    Currently I don't have a need for more sophisticated tool, might
    change this if I run into more complicated setups.

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ln -vsf $(pwd)/.profile ${HOME}/.profile
+
+for file in .config/*
+do
+    ln -vsf $(pwd)/$file ${HOME}/$file
+done
+
+for file in .local/*
+do
+    ln -vsf $(pwd)/$file ${HOME}/$file
+done
