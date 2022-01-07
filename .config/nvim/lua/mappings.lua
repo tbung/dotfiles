5c2e65ec9682a120e82980dc70f55a754b7d689e
Till Bungert <tillbungert@gmail.com>
2022-01-07 11:02:26 +0100

    nvim: switch to new keymap api

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,94 +1,155 @@
-local function map(mode, from, to, opt)
-    vim.api.nvim_set_keymap(mode, from, to, opt)
-end
-
-local opts = { noremap=true, silent=true }
+local opts = { noremap = true, silent = true }
 
 -- #######
 -- # LSP #
 -- #######
 
-map('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-map('n', '<leader>vdd', '<cmd>TroubleToggle document_diagnostics<CR>', opts)
-map('n', '<leader>vdw', '<cmd>TroubleToggle workspace_diagnostics<CR>', opts)
-map('n', '<leader>vf', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
-map('n', '<leader>vh', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-map('n', '<leader>vrn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-map('n', '<leader>vrr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-map('n', '<leader>vsd', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-map('n', '<leader>vsh', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-map('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-map('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-
+vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+vim.keymap.set("n", "<leader>vdd", "<cmd>TroubleToggle document_diagnostics<CR>", opts)
+vim.keymap.set("n", "<leader>vdw", "<cmd>TroubleToggle workspace_diagnostics<CR>", opts)
+vim.keymap.set("n", "<leader>vf", vim.lsp.buf.formatting, opts)
+vim.keymap.set("n", "<leader>vh", vim.lsp.buf.hover, opts)
+vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
+vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
+vim.keymap.set("n", "<leader>vsd", vim.diagnostic.open_float, opts)
+vim.keymap.set("n", "<leader>vsh", vim.lsp.buf.signature_help, opts)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
 
 -- #############
 -- # Telescope #
 -- #############
 
-map('n', '<C-p>', [[<cmd>lua require('telescope.builtin').find_files()<cr>]], opts)
-map('n', '<C-n>', [[<cmd>lua require('telescope.builtin').buffers({ sort_lastused = true, ignore_current_buffer = true })<cr>]], opts)
+vim.keymap.set("n", "<C-p>", function()
+  require("telescope.builtin").find_files()
+end, opts)
+vim.keymap.set("n", "<C-n>", function()
+  require("telescope.builtin").buffers({ sort_lastused = true, ignore_current_buffer = true })
+end, opts)
 
-map('n', '<leader>fb', [[<cmd>lua require('telescope.builtin').buffers({ sort_lastused = true, ignore_current_buffer = true })<cr>]], opts)
-map('n', '<leader>fca', [[<cmd>lua require('telescope.builtin').lsp_code_actions()<cr>]], opts)
-map('n', '<leader>fd', [[<cmd>lua require('telescope.builtin').find_files({ prompt_title = "dotfiles", shorten_path = false, cwd = "~/.dotfiles", hidden = true })<cr>]], opts)
-map('n', '<leader>fe', [[<cmd>lua require('telescope.builtin').file_browser()<cr>]], opts)
-map('n', '<leader>ff', [[<cmd>lua require('telescope.builtin').find_files()<cr>]], opts)
-map('n', '<leader>fg', [[<cmd>lua require('telescope.builtin').live_grep()<cr>]], opts)
-map('n', '<leader>fh', [[<cmd>lua require('telescope.builtin').help_tags()<cr>]], opts)
-map('n', '<leader>fp', [[<cmd>lua require('telescope').extensions.project.project({})<cr>]], opts)
-map('n', '<leader>fsw', [[<cmd>lua require('telescope.builtin').lsp_workspace_symbols()<cr>]], opts)
-map('n', '<leader>fsd', [[<cmd>lua require('telescope.builtin').lsp_document_symbols()<cr>]], opts)
-map('n', '<leader>fw', [[<cmd>lua require('telescope').extensions.git_worktree.git_worktrees()<cr>]], opts)
-map('n', '<leader>fz', [[<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown({winblend = 10, border = true, previewer = false, shorten_path = false}))<cr>]], opts)
-
+vim.keymap.set("n", "<leader>fb", function()
+  require("telescope.builtin").buffers({ sort_lastused = true, ignore_current_buffer = true })
+end, opts)
+vim.keymap.set("n", "<leader>fca", function()
+  require("telescope.builtin").lsp_code_actions()
+end, opts)
+vim.keymap.set("n", "<leader>fd", function()
+  require("telescope.builtin").find_files({
+    prompt_title = "dotfiles",
+    shorten_path = false,
+    cwd = "~/.dotfiles",
+    hidden = true,
+  })
+end, opts)
+vim.keymap.set("n", "<leader>fe", function()
+  require("telescope.builtin").file_browser()
+end, opts)
+vim.keymap.set("n", "<leader>ff", function()
+  require("telescope.builtin").find_files()
+end, opts)
+vim.keymap.set("n", "<leader>fg", function()
+  require("telescope.builtin").live_grep()
+end, opts)
+vim.keymap.set("n", "<leader>fh", function()
+  require("telescope.builtin").help_tags()
+end, opts)
+vim.keymap.set("n", "<leader>fp", function()
+  require("telescope").extensions.project.project({})
+end, opts)
+vim.keymap.set("n", "<leader>fsw", function()
+  require("telescope.builtin").lsp_workspace_symbols()
+end, opts)
+vim.keymap.set("n", "<leader>fsd", function()
+  require("telescope.builtin").lsp_document_symbols()
+end, opts)
+vim.keymap.set("n", "<leader>fw", function()
+  require("telescope").extensions.git_worktree.git_worktrees()
+end, opts)
+vim.keymap.set("n", "<leader>fz", function()
+  require("telescope.builtin").current_buffer_fuzzy_find(
+    require("telescope.themes").get_dropdown({ winblend = 10, border = true, previewer = false, shorten_path = false })
+  )
+end, opts)
 
 -- ###########
 -- # Harpoon #
 -- ###########
 
-map("n", "<leader>a", ":lua require('harpoon.mark').add_file()<CR>", opts)
-map("n", "<C-e>", ":lua require('harpoon.ui').toggle_quick_menu()<CR>", opts)
+vim.keymap.set("n", "<leader>a", function()
+  require("harpoon.mark").add_file()
+end, opts)
+vim.keymap.set("n", "<C-e>", function()
+  require("harpoon.ui").toggle_quick_menu()
+end, opts)
 
-map("n", "<C-h>", ":lua require('harpoon.ui').nav_file(1)<CR>", opts)
-map("n", "<C-j>", ":lua require('harpoon.ui').nav_file(2)<CR>", opts)
-map("n", "<C-k>", ":lua require('harpoon.ui').nav_file(3)<CR>", opts)
-map("n", "<C-l>", ":lua require('harpoon.ui').nav_file(4)<CR>", opts)
-map("n", "<leader>tf", ":lua require('harpoon.term').gotoTerminal(1)<CR>", opts)
-map("n", "<leader>td", ":lua require('harpoon.term').gotoTerminal(2)<CR>", opts)
-map("n", "<leader>cf", ":lua require('harpoon.term').sendCommand(1, 1, vim.fn.expand('%'));require('harpoon.term').gotoTerminal(1)<CR>", opts)
-map("n", "<leader>cd", ":lua require('harpoon.term').sendCommand(1, 2, vim.fn.expand('%'));require('harpoon.term').gotoTerminal(1)<CR>", opts)
-map("n", "<leader>cc", ":lua require('harpoon.cmd-ui').toggle_quick_menu()<CR>", opts)
-
+vim.keymap.set("n", "<C-h>", function()
+  require("harpoon.ui").nav_file(1)
+end, opts)
+vim.keymap.set("n", "<C-j>", function()
+  require("harpoon.ui").nav_file(2)
+end, opts)
+vim.keymap.set("n", "<C-k>", function()
+  require("harpoon.ui").nav_file(3)
+end, opts)
+vim.keymap.set("n", "<C-l>", function()
+  require("harpoon.ui").nav_file(4)
+end, opts)
+vim.keymap.set("n", "<leader>tf", function()
+  require("harpoon.term").gotoTerminal(1)
+end, opts)
+vim.keymap.set("n", "<leader>td", function()
+  require("harpoon.term").gotoTerminal(2)
+end, opts)
+vim.keymap.set("n", "<leader>cf", function()
+  require("harpoon.term").sendCommand(1, 1, vim.fn.expand("%"))
+  require("harpoon.term").gotoTerminal(1)
+end, opts)
+vim.keymap.set("n", "<leader>cd", function()
+  require("harpoon.term").sendCommand(1, 2, vim.fn.expand("%"))
+  require("harpoon.term").gotoTerminal(1)
+end, opts)
+vim.keymap.set("n", "<leader>cc", function()
+  require("harpoon.cmd-ui").toggle_quick_menu()
+end, opts)
 
 -- #########
 -- # Magma #
 -- #########
 
-map("n", "<leader>r", "nvim_exec('MagmaEvaluateOperator', v:true)", vim.tbl_extend("force", opts, {expr = true}))
-map("n", "<leader>rr", ":MagmaEvaluateLine<CR>", opts)
-map("x", "<leader>r", ":<C-u>MagmaEvaluateVisual<CR>", opts)
-map("n", "<leader>rc", ":MagmaReevaluateCell<CR>", opts)
-map("n", "<leader>rd", ":MagmaDelete<CR>", opts)
-map("n", "<leader>ro", ":MagmaShowOutput<CR>", opts)
-
+vim.keymap.set(
+  "n",
+  "<leader>r",
+  "nvim_exec('MagmaEvaluateOperator', v:true)",
+  vim.tbl_extend("force", opts, { expr = true })
+)
+vim.keymap.set("n", "<leader>rr", ":MagmaEvaluateLine<CR>", opts)
+vim.keymap.set("x", "<leader>r", ":<C-u>MagmaEvaluateVisual<CR>", opts)
+vim.keymap.set("n", "<leader>rc", ":MagmaReevaluateCell<CR>", opts)
+vim.keymap.set("n", "<leader>rd", ":MagmaDelete<CR>", opts)
+vim.keymap.set("n", "<leader>ro", ":MagmaShowOutput<CR>", opts)
 
 -- #######
 -- # Git #
 -- #######
-map("n", "<leader>gg", ":lua git_toggle()<CR>", opts)
-map("n", "<leader>gp", ":lua PushCluster()<CR>", opts)
-
+-- vim.keymap.set("n", "<leader>gg", function() git_toggle() end, opts)
+-- vim.keymap.set("n", "<leader>gp", function() PushCluster() end, opts)
 
 -- ########
 -- # Misc #
 -- ########
 
 -- Maximizer
-map('n', '<leader>m', [[<cmd>MaximizerToggle!<cr>]], opts)
+vim.keymap.set("n", "<leader>m", [[<cmd>MaximizerToggle!<cr>]], opts)
 -- Persistence
-map("n", "<leader>qs", [[<cmd>lua require("persistence").load()<cr>]], opts)
-map("n", "<leader>ql", [[<cmd>lua require("persistence").load({ last = true })<cr>]], opts)
-map("n", "<leader>qd", [[<cmd>lua require("persistence").stop()<cr>]], opts)
+vim.keymap.set("n", "<leader>qs", function()
+  require("persistence").load()
+end, opts)
+vim.keymap.set("n", "<leader>ql", function()
+  require("persistence").load({ last = true })
+end, opts)
+vim.keymap.set("n", "<leader>qd", function()
+  require("persistence").stop()
+end, opts)
 -- Nvim Tree
-map("n", "<leader>n", [[<Cmd>NvimTreeToggle<CR>]], opts)
+vim.keymap.set("n", "<leader>n", [[<Cmd>NvimTreeToggle<CR>]], opts)
