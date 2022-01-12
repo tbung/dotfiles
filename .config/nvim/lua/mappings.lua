7e8fd21f1b5d35851054b8e8eafbf01227c456fb
Till Bungert <tillbungert@gmail.com>
2022-01-12 22:41:38 +0100

    nvim(snip): add luasnip

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -159,6 +159,9 @@ vim.keymap.set("n", "<leader>ro", ":MagmaShowOutput<CR>", opts)
 -- # Misc #
 -- ########
 
+-- LuaSnip
+vim.api.nvim_set_keymap("i", "<C-E>", "<Plug>luasnip-expand-or-jump", {})
+vim.api.nvim_set_keymap("s", "<C-E>", "<Plug>luasnip-expand-or-jump", {})
 -- Maximizer
 vim.keymap.set("n", "<leader>m", [[<cmd>MaximizerToggle!<cr>]], opts)
 -- Persistence
--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -7,49 +7,73 @@ cmp.setup({
     end,
   },
   mapping = {
-    ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-    ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-    ["<Down>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
-    ["<Up>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
-    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
-    ["<C-e>"] = cmp.mapping.close(),
-    ["<CR>"] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
+    ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+    ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+    ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+    ["<C-y>"] = cmp.config.disable,
+    ["<C-e>"] = cmp.mapping({
+      i = cmp.mapping.abort(),
+      c = cmp.mapping.close(),
     }),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
   },
-  sources = {
+  sources = cmp.config.sources({
     { name = "nvim_lsp" },
-    { name = "path" },
+    { name = "luasnip" }, -- For luasnip users.
+  }, {
+    { name = "neorg" },
     { name = "buffer" },
     { name = "nvim_lua" },
-    { name = "luasnip" },
     { name = "spell" },
     {
       name = "dictionary",
       keyword_length = 2,
     },
-  },
-  formatting = {
-    format = require("lspkind").cmp_format(),
-  },
-  experimental = {
-    ghost_text = true,
+  }),
+  sorting = {
+    comparators = {
+      cmp.config.compare.offset,
+      cmp.config.compare.exact,
+      cmp.config.compare.score,
+      require("cmp-under-comparator").under,
+      cmp.config.compare.kind,
+      cmp.config.compare.sort_text,
+      cmp.config.compare.length,
+      cmp.config.compare.order,
+    },
   },
 })
+
+-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+-- cmp.setup.cmdline("/", {
+--   sources = cmp.config.sources({
+--     { name = "nvim_lsp_document_symbol" },
+--   }, {
+--     { name = "buffer" },
+--   }),
+-- })
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+-- cmp.setup.cmdline(":", {
+--   sources = cmp.config.sources({
+--     { name = "path" },
+--   }, {
+--     { name = "cmdline" },
+--   }),
+-- })
 
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
 
 require("cmp_dictionary").setup({
   dic = {
-    ["*"] = {"/usr/lib/aspell/en.dat", "/usr/lib/aspell/de_DE.dat"},
+    ["*"] = { "/usr/lib/aspell/en.dat", "/usr/lib/aspell/de_DE.dat" },
   },
   -- The following are default values, so you don't need to write them if you don't want to change them
   exact = 2,
-  async = false,
+  async = true,
   capacity = 5,
   debug = false,
 })
+
+require("luasnip/loaders/from_vscode").lazy_load()
