3a7c4dbe91a272c31b1824a21965b0ecabad0698
Till Bungert <tillbungert@gmail.com>
2022-03-13 15:58:40 +0100

    nvim: enable mouse
    Not for editing, but for showing to others and easier reading with a
    touchpad.

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -20,7 +20,7 @@ vim.opt.spelllang = "en"
 vim.opt.completeopt = "menuone,noinsert,noselect"
 vim.cmd([[set shortmess+=c]])
 vim.opt.wildoptions = "tagfile"
-vim.opt.mouse = "n"
+vim.opt.mouse = "a"
 vim.g.do_filetype_lua = 1
 vim.g.did_load_filetypes = 0
 
