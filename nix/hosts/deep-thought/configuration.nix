81c52ab674eb5b725ca3c200df0c7cbf15b2e456
Till Bungert <tillbungert@gmail.com>
2022-03-17 01:37:07 +0100

    nix: use community binary cache

--- nix/hosts/deep-thought/configuration.nix
+++ nix/hosts/deep-thought/configuration.nix
@@ -106,6 +106,15 @@
     extraOptions = ''
       experimental-features = nix-command flakes
     '';
+    settings = {
+      substituters = [
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+      auto-optimise-store = true;
+    };
   };
 
   nixpkgs.config = {
