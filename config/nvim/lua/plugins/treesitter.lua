6d7ef385ef3cb05ea64def55036b71dbd44f9796
Till Bungert <tillbungert@gmail.com>
2022-07-13 16:21:52 +0200

    nvim: treesitter rainbow

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -59,7 +59,7 @@ require("nvim-treesitter.configs").setup({
     },
   },
   rainbow = {
-    enable = false,
+    enable = true,
     extended_mode = false, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
     max_file_lines = nil, -- Do not enable for files with more than 1000 lines, int
   },
