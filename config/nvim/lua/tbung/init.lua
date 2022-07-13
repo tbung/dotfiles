6d82bdc6389d5cda00fdb6d1050cba0b9bc86f49
Till Bungert <tillbungert@gmail.com>
2022-07-13 16:22:22 +0200

    nvim: summing utility

--- config/nvim/lua/tbung/init.lua
+++ config/nvim/lua/tbung/init.lua
@@ -13,7 +13,7 @@ end
 
 M.update_ssh_env_from_tmux = function()
   if vim.env.TMUX then
-    local _env = Job:new({command = "tmux", args = {"show-environment"}}):sync()
+    local _env = Job:new({ command = "tmux", args = { "show-environment" } }):sync()
     local env = {}
 
     for _, value in pairs(_env) do
@@ -25,6 +25,27 @@ M.update_ssh_env_from_tmux = function()
     vim.env.SSH_CONNECTION = env.SSH_CONNECTION
     vim.env.DISPLAY = env.DISPLAY
   end
+end
+
+--- Sum integers in a table
+---@param numbers number[]
+---@return number
+M.sum = function(numbers)
+  local s = 0
+
+  for _, n in ipairs(numbers) do
+    s = s + n
+  end
+
+  return s
+end
+
+--- Sum over yanked numbers, separated by line
+M.sum_yanked = function()
+  tbl = vim.split(vim.fn.getreg('"0'), "\n")
+  tbl = vim.tbl_map(function(x) return tonumber(x) end, tbl)
+  tbl = vim.tbl_filter(function(x) return x ~= nil end, tbl)
+  return M.sum(tbl)
 end
 
 return M
