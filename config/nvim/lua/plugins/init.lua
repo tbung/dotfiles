3d28adce9c01ae092bc7d1988ed82d07751a9148
Till Bungert <tillbungert@gmail.com>
2022-09-13 12:58:11 +0200

    nvim: bunch of new plugins

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -133,12 +133,18 @@ return require("packer").startup({
     use("jose-elias-alvarez/null-ls.nvim")
 
     use("onsails/lspkind-nvim")
+    -- use({
+    --   "filipdutescu/renamer.nvim",
+    --   branch = "master",
+    --   requires = "nvim-lua/plenary.nvim",
+    --   config = function()
+    --     require("renamer").setup({})
+    --   end,
+    -- })
     use({
-      "filipdutescu/renamer.nvim",
-      branch = "master",
-      requires = "nvim-lua/plenary.nvim",
+      "smjonas/inc-rename.nvim",
       config = function()
-        require("renamer").setup({})
+        require("inc_rename").setup({ input_buffer_type = "dressing" })
       end,
     })
     use({
@@ -332,13 +338,13 @@ return require("packer").startup({
       end,
     })
 
-    use({
-      "rcarriga/nvim-notify",
-      config = function()
-        require("notify").setup()
-        vim.notify = require("notify")
-      end,
-    })
+    -- use({
+    --   "rcarriga/nvim-notify",
+    --   config = function()
+    --     require("notify").setup()
+    --     vim.notify = require("notify")
+    --   end,
+    -- })
 
     use({
       "nvim-neo-tree/neo-tree.nvim",
@@ -528,12 +534,12 @@ return require("packer").startup({
       end,
     })
 
-    use({
-      "j-hui/fidget.nvim",
-      config = function()
-        require("fidget").setup({})
-      end,
-    })
+    -- use({
+    --   "j-hui/fidget.nvim",
+    --   config = function()
+    --     require("fidget").setup({})
+    --   end,
+    -- })
 
     use({
       "mickael-menu/zk-nvim",
@@ -619,6 +625,79 @@ return require("packer").startup({
           provider_selector = function(bufnr, filetype, buftype)
             return { "treesitter", "indent" }
           end,
+        })
+      end,
+    })
+
+    use({
+      "max397574/colortils.nvim",
+      cmd = "Colortils",
+      config = function()
+        require("colortils").setup()
+      end,
+    })
+
+    use({
+      "levouh/tint.nvim",
+      config = function()
+        -- Default configuration
+        require("tint").setup()
+      end,
+    })
+
+    use({ "stevearc/dressing.nvim" })
+
+    use({
+      "vigoux/notifier.nvim",
+      config = function()
+        require("notifier").setup({
+          -- You configuration here
+        })
+      end,
+    })
+
+    use({
+      "hkupty/iron.nvim",
+      config = function()
+        local iron = require("iron.core")
+
+        iron.setup({
+          config = {
+            -- Whether a repl should be discarded or not
+            scratch_repl = true,
+            -- Your repl definitions come here
+            repl_definition = {
+              sh = {
+                command = { "zsh" },
+              },
+            },
+            -- How the repl window will be displayed
+            -- See below for more information
+            repl_open_cmd = require("iron.view").right(function()
+              return vim.o.columns / 3
+            end),
+          },
+          -- Iron doesn't set keymaps by default anymore.
+          -- You can set them here or manually add keymaps to the functions in iron.core
+          keymaps = {
+            send_motion = "<space>sc",
+            visual_send = "<space>sc",
+            send_file = "<space>sf",
+            send_line = "<space>sl",
+            send_mark = "<space>sm",
+            mark_motion = "<space>mc",
+            mark_visual = "<space>mc",
+            remove_mark = "<space>md",
+            cr = "<space>s<cr>",
+            interrupt = "<space>s<space>",
+            exit = "<space>sq",
+            clear = "<space>cl",
+          },
+          -- If the highlight is on, you can change how it looks
+          -- For the available options, check nvim_set_hl
+          highlight = {
+            italic = true,
+          },
         })
       end,
     })
