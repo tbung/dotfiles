95e6e5c50a95b113e4e4909cc33f98603459d61e
Till Bungert <tillbungert@gmail.com>
2022-12-21 14:03:42 +0100

    nvim: add peek

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -513,7 +513,15 @@ return require("packer").startup({
     use({
       "danymat/neogen",
       config = function()
-        require("neogen").setup({})
+        require("neogen").setup({
+          languages = {
+            python = {
+              template = {
+                annotation_convention = "reST",
+              },
+            },
+          },
+        })
       end,
       requires = "nvim-treesitter/nvim-treesitter",
     })
@@ -704,6 +712,34 @@ return require("packer").startup({
           },
           enable_highlighting = true,
         })
+      end,
+    })
+
+    use({
+      "toppair/peek.nvim",
+      run = "deno task --quiet build:fast",
+      config = function()
+        -- default config:
+        require("peek").setup({
+          auto_load = true, -- whether to automatically load preview when
+          -- entering another markdown buffer
+          close_on_bdelete = true, -- close preview window on buffer delete
+
+          syntax = true, -- enable syntax highlighting, affects performance
+
+          theme = "dark", -- 'dark' or 'light'
+
+          update_on_change = true,
+
+          -- relevant if update_on_change is true
+          throttle_at = 200000, -- start throttling when file exceeds this
+          -- amount of bytes in size
+          throttle_time = "auto", -- minimum amount of time in milliseconds
+          -- that has to pass before starting new render
+        })
+
+        vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+        vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
       end,
     })
   end,
