420d22b9411153efccbf0e045b8ae41e4f432fbc
Till Bungert <tillbungert@gmail.com>
2023-01-11 11:39:57 +0100

    nvim: switch to lazy plugin manager

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,8 +1,1 @@
-local ok, impatient = pcall(require, "impatient")
-
 require("tillb")
-
-local ok, _ = pcall(require, "packer_compiled")
-if not ok then
-  print("Plugins not installed")
-end
--- config/nvim/lua/tillb/plugins.lua
+++ config/nvim/lua/tillb/plugins.lua
@@ -1,167 +1,161 @@
 local execute = vim.api.nvim_command
 local fn = vim.fn
 
-if not pcall(require, "packer") then
-  local install_path = fn.stdpath("data") .. "/site/pack/packer/opt/packer.nvim"
-
-  if fn.empty(fn.glob(install_path)) > 0 then
-    execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
-    execute("packadd packer.nvim")
-  end
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
-vim.cmd([[packadd packer.nvim]])
-
-return require("packer").startup({
-  function(use)
-    -- Packer can manage itself
-    use("wbthomason/packer.nvim")
-
-    use("lewis6991/impatient.nvim")
-
-    use({
-      "nvim-telescope/telescope.nvim",
-      tag = "0.1.0",
-      requires = { { "nvim-lua/plenary.nvim" } },
-    })
-
-    use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
-
-    use({
-      "catppuccin/nvim",
-      as = "catppuccin",
-    })
-
-    use("mbbill/undotree")
-    use("tpope/vim-fugitive")
-    use({
-      "lewis6991/gitsigns.nvim",
-      config = function()
-        require("gitsigns").setup({
-          attach_to_untracked = true,
-        })
-      end,
-    })
-    use("tpope/vim-repeat")
-    use("tpope/vim-eunuch")
-    use("wellle/targets.vim")
-    use({
-      "numToStr/Comment.nvim",
-      config = function()
-        require("Comment").setup()
-      end,
-    })
-    use({
-      "kylechui/nvim-surround",
-      config = function()
-        require("nvim-surround").setup({})
-      end,
-    })
-    use({
-      "jinh0/eyeliner.nvim",
-      config = function()
-        require("eyeliner").setup({
-          highlight_on_key = true,
-        })
-      end,
-    })
-
-    use({
-      "VonHeikemen/lsp-zero.nvim",
-      requires = {
-        -- LSP Support
-        { "neovim/nvim-lspconfig" },
-        { "williamboman/mason.nvim" },
-        { "williamboman/mason-lspconfig.nvim" },
-
-        -- Autocompletion
-        { "hrsh7th/nvim-cmp" },
-        { "hrsh7th/cmp-buffer" },
-        { "hrsh7th/cmp-path" },
-        { "saadparwaiz1/cmp_luasnip" },
-        { "hrsh7th/cmp-nvim-lsp" },
-        { "hrsh7th/cmp-nvim-lua" },
-
-        -- Snippets
-        { "L3MON4D3/LuaSnip" },
-        { "rafamadriz/friendly-snippets" },
-      },
-    })
-    use("jose-elias-alvarez/null-ls.nvim")
-    use({ "WhoIsSethDaniel/mason-tool-installer.nvim" })
-
-    use({
-      "mfussenegger/nvim-dap",
-    })
-    use("rcarriga/nvim-dap-ui")
-    use("theHamsta/nvim-dap-virtual-text")
-    use("mfussenegger/nvim-dap-python")
-    use("nvim-telescope/telescope-dap.nvim")
-
-    use({
-      "lukas-reineke/indent-blankline.nvim",
-      config = function()
-        require("indent_blankline").setup({})
-      end,
-    })
-
-    use({
-      "hkupty/iron.nvim",
-    })
-
-    use({
-      "karb94/neoscroll.nvim",
-    })
-
-    use({
-      "echasnovski/mini.nvim",
-    })
-
-    use({
-      "nvim-neo-tree/neo-tree.nvim",
-      branch = "v2.x",
-      requires = {
-        "nvim-lua/plenary.nvim",
-        "kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
-        "MunifTanjim/nui.nvim",
-      },
-    })
-
-    use({
-      "vigoux/notifier.nvim",
-      config = function()
-        require("notifier").setup({})
-      end,
-    })
-
-    use({
-      "feline-nvim/feline.nvim",
-      after = "catppuccin",
-      requires = "kyazdani42/nvim-web-devicons",
-    })
-
-    use({
-      "folke/todo-comments.nvim",
-      requires = "nvim-lua/plenary.nvim",
-      config = function()
-        require("todo-comments").setup({})
-      end,
-    })
-
-    use({
-      "ThePrimeagen/harpoon",
-    })
-
-    use({
-      "folke/which-key.nvim",
-      config = function()
-        require("which-key").setup({})
-      end,
-    })
-  end,
-  config = {
-    -- Move to lua dir so impatient.nvim can cache it
-    compile_path = vim.fn.stdpath("config") .. "/lua/packer_compiled.lua",
-    max_jobs = 8,
+local plugins = {
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.0",
+    dependencies = { { "nvim-lua/plenary.nvim" } },
   },
-})
+
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000,
+  },
+
+  "mbbill/undotree",
+  "tpope/vim-fugitive",
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup({
+        attach_to_untracked = true,
+      })
+    end,
+  },
+  "tpope/vim-repeat",
+  "tpope/vim-eunuch",
+  "wellle/targets.vim",
+  {
+    "numToStr/Comment.nvim",
+    config = function()
+      require("Comment").setup()
+    end,
+  },
+  {
+    "kylechui/nvim-surround",
+    config = function()
+      require("nvim-surround").setup({})
+    end,
+  },
+  {
+    "jinh0/eyeliner.nvim",
+    config = function()
+      require("eyeliner").setup({
+        highlight_on_key = true,
+      })
+    end,
+  },
+
+  {
+    "VonHeikemen/lsp-zero.nvim",
+    dependencies = {
+      -- LSP Support
+      { "neovim/nvim-lspconfig" },
+      { "williamboman/mason.nvim" },
+      { "williamboman/mason-lspconfig.nvim" },
+
+      -- Autocompletion
+      { "hrsh7th/nvim-cmp" },
+      { "hrsh7th/cmp-buffer" },
+      { "hrsh7th/cmp-path" },
+      { "saadparwaiz1/cmp_luasnip" },
+      { "hrsh7th/cmp-nvim-lsp" },
+      { "hrsh7th/cmp-nvim-lua" },
+
+      -- Snippets
+      { "L3MON4D3/LuaSnip" },
+      { "rafamadriz/friendly-snippets" },
+    },
+  },
+  "jose-elias-alvarez/null-ls.nvim",
+  { "WhoIsSethDaniel/mason-tool-installer.nvim" },
+
+  {
+    "mfussenegger/nvim-dap",
+  },
+  "rcarriga/nvim-dap-ui",
+  "theHamsta/nvim-dap-virtual-text",
+  "mfussenegger/nvim-dap-python",
+  "nvim-telescope/telescope-dap.nvim",
+
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    config = function()
+      require("indent_blankline").setup({})
+    end,
+  },
+
+  {
+    "hkupty/iron.nvim",
+  },
+
+  {
+    "karb94/neoscroll.nvim",
+  },
+
+  {
+    "echasnovski/mini.nvim",
+  },
+
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v2.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    },
+  },
+
+  {
+    "vigoux/notifier.nvim",
+    config = function()
+      require("notifier").setup({})
+    end,
+  },
+
+  {
+    "feline-nvim/feline.nvim",
+    after = "catppuccin",
+    dependencies = "kyazdani42/nvim-web-devicons",
+  },
+
+  {
+    "folke/todo-comments.nvim",
+    dependencies = "nvim-lua/plenary.nvim",
+    config = function()
+      require("todo-comments").setup({})
+    end,
+  },
+
+  {
+    "ThePrimeagen/harpoon",
+  },
+
+  {
+    "folke/which-key.nvim",
+    config = function()
+      require("which-key").setup({})
+    end,
+  },
+}
+
+require("lazy").setup(plugins)
