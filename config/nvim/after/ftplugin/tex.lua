4030e31ebeb50ad43d2621ceaf69ffad8c8c7bdc
Till Bungert <tillbungert@gmail.com>
2023-07-19 15:11:18 +0200

    feat(nvim): use softwrapping in tex files

--- config/nvim/after/ftplugin/tex.lua
+++ config/nvim/after/ftplugin/tex.lua
@@ -1,1 +1,6 @@
-vim.opt_local.textwidth = 110
+vim.opt_local.wrap = true
+vim.opt_local.linebreak = true
+vim.opt_local.breakindent = true
+
+vim.keymap.set("n", "j", "gj", { buffer = true })
+vim.keymap.set("n", "k", "gk", { buffer = true })
