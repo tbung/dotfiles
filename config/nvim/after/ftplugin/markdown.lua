cb41f4a19f98d2bf58daa1a31334175ec3275783
Till Bungert <tillbungert@gmail.com>
2023-07-21 14:29:51 +0200

    feat(nvim): use softwrapping in markdown files

---
+++ config/nvim/after/ftplugin/markdown.lua
@@ -0,0 +1,7 @@
+vim.opt_local.wrap = true
+vim.opt_local.linebreak = true
+vim.opt_local.breakindent = true
+vim.opt_local.breakindentopt = "shift:4"
+
+vim.keymap.set("n", "j", "gj", { buffer = true })
+vim.keymap.set("n", "k", "gk", { buffer = true })
