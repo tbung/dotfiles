1ab5f247ca290d9b0a82bc182055afce0eb3152e
Till Bungert <tillbungert@gmail.com>
2023-08-09 12:24:55 +0200

    chore(nvim): remove unused chatgpt plugin

--- config/nvim/lua/tillb/plugins/ai.lua
+++ config/nvim/lua/tillb/plugins/ai.lua
@@ -16,16 +16,4 @@ return {
       require("copilot_cmp").setup()
     end,
   },
-  {
-    "jackMort/ChatGPT.nvim",
-    cmd = "ChatGPT",
-    config = function()
-      require("chatgpt").setup()
-    end,
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-    },
-  },
 }
