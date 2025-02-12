167e7aaccc2c485073bfaf5898103590676982fe
Till Bungert <tillbungert@gmail.com>
2025-02-12 02:12:35 +0100

    fix(nvim): c-bs should delete word in snacks

--- config/nvim/lua/tillb/plugins/telescope.lua
+++ config/nvim/lua/tillb/plugins/telescope.lua
@@ -5,19 +5,36 @@ return {
     lazy = false,
     ---@type snacks.Config
     opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
       bigfile = { enabled = true },
       dashboard = { enabled = false },
       indent = { enabled = false },
-      input = { enabled = true },
-      picker = { enabled = true },
+      input = {
+        enabled = true,
+        win = {
+          keys = {
+            i_ctrl_bs = { "<C-BS>", "delete_word", mode = "i", expr = true },
+          },
+          actions = {
+            delete_word = function()
+              return "<c-s-w>"
+            end,
+          },
+        },
+      },
+      picker = {
+        enabled = true,
+        win = {
+          input = {
+            keys = {
+              ["<C-BS>"] = { "<c-s-w>", mode = "i", expr = true },
+            },
+          },
+        },
+      },
       notifier = { enabled = true },
       quickfile = { enabled = true },
-      scroll = { enabled = false },
-      statuscolumn = { enabled = false },
       words = { enabled = true },
+      image = { enabled = true },
     },
   },
 }
