2ee7974a83eda8127f41e2074265e4143653ac54
Till Bungert <tillbungert@gmail.com>
2025-05-27 03:28:02 +0200

    feat(nvim): auto-install treesitter parsers

--- config/nvim/lua/tillb/plugins/treesitter.lua
+++ config/nvim/lua/tillb/plugins/treesitter.lua
@@ -11,21 +11,23 @@ return {
     lazy = false,
     branch = "main",
     init = function()
-      vim.api.nvim_create_autocmd('User', {
-        pattern = 'TSUpdate',
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "TSUpdate",
         callback = function()
-          require('nvim-treesitter.parsers').markdown.install_info.url = nil
-          require('nvim-treesitter.parsers').markdown_inline.install_info.url = nil
-          require('nvim-treesitter.parsers').markdown.install_info.path = vim.fn.expand(
-          "$HOME/Projects/tree-sitter-markdown/")
-          require('nvim-treesitter.parsers').markdown_inline.install_info.path = vim.fn.expand(
-          "$HOME/Projects/tree-sitter-markdown/")
+          require("nvim-treesitter.parsers").markdown.install_info.url = nil
+          require("nvim-treesitter.parsers").markdown_inline.install_info.url = nil
+          require("nvim-treesitter.parsers").markdown.install_info.path = vim.fn.expand(
+            "$HOME/Projects/tree-sitter-markdown/")
+          require("nvim-treesitter.parsers").markdown_inline.install_info.path = vim.fn.expand(
+            "$HOME/Projects/tree-sitter-markdown/")
         end,
       })
       vim.api.nvim_create_autocmd("FileType", {
         callback = function(args)
-          pcall(vim.treesitter.start, args.buf)
-        end
+          require("nvim-treesitter").install({ vim.treesitter.language.get_lang(vim.bo.filetype) }):await(function()
+            pcall(vim.treesitter.start, args.buf)
+          end)
+        end,
       })
     end,
   },
