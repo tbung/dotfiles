a4e27bd88a89a0218fc7cbd2a20016b484db5235
Till Bungert <tillbungert@gmail.com>
2025-08-16 19:28:26 +0200

    fix(nvim): remove custom md treesitter parser

--- config/nvim/lua/tillb/plugins/treesitter.lua
+++ config/nvim/lua/tillb/plugins/treesitter.lua
@@ -3,18 +3,6 @@ vim.pack.add({
   "https://github.com/nvim-treesitter/nvim-treesitter-context",
 })
 
-vim.api.nvim_create_autocmd("User", {
-  pattern = "TSUpdate",
-  callback = function()
-    require("nvim-treesitter.parsers").markdown.install_info.url = nil
-    require("nvim-treesitter.parsers").markdown_inline.install_info.url = nil
-    require("nvim-treesitter.parsers").markdown.install_info.path = vim.fn.expand(
-      "$HOME/Projects/tree-sitter-markdown/")
-    require("nvim-treesitter.parsers").markdown_inline.install_info.path = vim.fn.expand(
-      "$HOME/Projects/tree-sitter-markdown/")
-  end,
-})
-
 vim.api.nvim_create_autocmd("FileType", {
   callback = function(args)
     require("nvim-treesitter").install({ vim.treesitter.language.get_lang(vim.bo.filetype) }):await(function()
