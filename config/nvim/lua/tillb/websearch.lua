f910a9bae5ddb10f0483698590bdb7cef1e99cb8
Till Bungert <tillbungert@gmail.com>
2025-12-12 23:32:16 +0100

    feat(nvim): searxng instead of google

--- config/nvim/lua/tillb/websearch.lua
+++ config/nvim/lua/tillb/websearch.lua
@@ -1,30 +1,38 @@
 local M = {}
 
----@enum search_engines
+---@class tillb.websearch.Engine
+---@field name string
+---@field url string
+
+---@type tillb.websearch.Engine[]
 M.search_engines = {
-  ["Python"] = "https://docs.python.org/3/search.html?q=%s",
-  ["Wikipedia"] = "https://en.wikipedia.org/wiki/Special:Search?search=%s",
-  ["Google"] = "https://google.com/search?q=%s",
-  ["Arch Wiki"] = "https://wiki.archlinux.org/index.php?search=%s&title=Special%%3ASearch&wprov=acrw1_-1",
+  {name = "SearXNG", url = "https://sx.cloud.tbung.de/search?q=%s", },
+  {name = "Wikipedia", url = "https://en.wikipedia.org/wiki/Special:Search?search=%s", },
+  {name = "Python", url = "https://docs.python.org/3/search.html?q=%s", },
+  {name = "Arch Wiki", url = "https://wiki.archlinux.org/index.php?search=%s&title=Special%%3ASearch&wprov=acrw1_-1", },
 }
 
----@param engine search_engines
+---@param engine tillb.websearch.Engine
 ---@param query string
 function M.search(engine, query)
-  vim.ui.open(string.format(engine, query))
+  vim.ui.open(string.format(engine.url, query))
 end
 
+---@param query string
 function M.search_with(query)
   if not query then
     return
   end
-  vim.ui.select(vim.tbl_keys(M.search_engines), { prompt = "Search With" }, function(choice)
+  vim.ui.select(M.search_engines, { prompt = "Search With", format_item = function (item)
+    return item.name
+  end }, function(choice)
     if choice then
-      M.search(M.search_engines[choice], query)
+      M.search(choice, query)
     end
   end)
 end
 
+---@param query string
 function M.interactive(query)
   if not query then
     vim.ui.input({ prompt = "Search:" }, M.search_with)
